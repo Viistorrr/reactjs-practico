16b1b12076f48c5e7647f56047d14e6aab9a242e
viistorrr <victormeza41@gmail.com>
2021-11-10 14:07:53 -0600

    customHook getProducts

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,9 +3,11 @@ import '../styles/ProductItem.scss';
 
 import addToCart from '@icons/bt_add_to_cart.svg'
 
-const ProductItem = () => {
+const ProductItem = ({product}) => {
 
 	const [cart, setCart] = useState([])
+
+	const {title, price, description} = product
 
 	const handleClick = () =>{
 		setCart([])
@@ -13,11 +15,11 @@ const ProductItem = () => {
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${price}</p>
+					<p>{title}</p>
 				</div>
 				<figure onClick={handleClick}>
 					<img src={addToCart} alt="" />
